b3fb36a828a58fc2c4366f0c6b17cfd937c865a3
akshaykishore <akshaykishore@qburst.com>
2018-10-22 20:37:51 +0530

    Backing up

---
+++ js/page1.js
@@ -0,0 +1,5 @@
+document.getElementById("colour-bar").addEventListener("click", function(){
+    $("#info-row").toggle();
+    $("#colour-info").toggleClass("unhide");
+    console.log('hello');
+});
